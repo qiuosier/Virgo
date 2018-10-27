69e1f92091437baa597981aa63ed3bc1b4ca1882
Qiu <qiuosier@gmail.com>
2018-10-26 22:13:49 -0400

    Move TimeSeries to technical module.

--- virgo_stock/stock.py
+++ virgo_stock/stock.py
@@ -1,8 +1,8 @@
 import datetime
 import pandas as pd
-import numpy as np
 from collections import OrderedDict
 from virgo_stock.source import DataSourceInterface
+from virgo_stock.technical import TimeSeries
 
 
 class DataPoint:
@@ -64,66 +64,6 @@ class DataPoint:
             if p.low < val_low:
                 val_low = p.low
         return cls(timestamp, val_open, val_high, val_low, val_close, volume)
-
-
-class TimeSeries:
-    """Represents stock time series data.
-
-    Attributes:
-        df: A pandas data frame including columns of timestamp, open, high, low, close and volume.
-
-    """
-    def __init__(self, data_frame):
-        """Initializes the TimeSeries with a pandas data frame.
-
-        Args:
-            data_frame: A pandas data frame including columns of timestamp, open, high, low, close and volume.
-
-        """
-        self.df = data_frame
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, item):
-        """Extends the slices
-
-        See https://docs.python.org/2.3/whatsnew/section-slices.html
-        Args:
-            item:
-
-        Returns:
-
-        """
-        if isinstance(item, str):
-            return self.df[item]
-        else:
-            return TimeSeries(self.df[item])
-
-    def sma(self, n=10, series_type='close', column_name=None):
-        """Simple Moving Average
-
-        Args:
-            n: Number of data points used to calculate each moving average value.
-            series_type: The price type used to calculate the moving average.
-            column_name:
-
-        Returns:
-
-        """
-        values = [None] * len(self.df)
-        ret = np.cumsum(np.flip(self.df[series_type].values, 0), dtype=float)
-        ret[n:] = ret[n:] - ret[:-n]
-        values[n - 1:] = ret[n - 1:] / n
-        values.reverse()
-        if column_name is None:
-            column_name = 'sma_%s' % n
-        df = pd.DataFrame(OrderedDict([
-            ('timestamp', self.df['timestamp'].copy()),
-            (column_name, pd.Series(values, index=self.df.index)),
-        ]))
-        df.set_index('timestamp', inplace=True)
-        return df
 
 
 class Stock:
