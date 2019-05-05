38405d116066c8dc2a8ada265ce67180860121b2
Qiu <qiuosier@gmail.com>
2019-05-05 16:09:13 -0400

    Add test data.

---
+++ tests/test_stock.py
@@ -0,0 +1,14 @@
+"""Contains tests for the stock module.
+"""
+import datetime
+import unittest
+
+import os
+import sys
+
+
+class TestStock(unittest.TestCase):
+    def test_str_methods(self):
+        """Tests calling methods of python str with AString
+        """
+        pass
