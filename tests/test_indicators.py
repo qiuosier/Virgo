7e765e5004f0ee2837300db8397fda3022aea3fa
Qiu <qiuosier@gmail.com>
2019-05-06 22:00:02 -0400

    Update indicators and add tests.

---
+++ tests/test_indicators.py
@@ -0,0 +1,72 @@
+"""Contains tests for the indicators module.
+"""
+import datetime
+import unittest
+import pandas as pd
+
+import os
+import sys
+from tests.base import TestWithAlphaVantage
+from virgo_stock.stock import Stock
+from virgo_stock.indicators import SMA, EMA
+
+
+class TestIndicators(TestWithAlphaVantage):
+    def assert_set_values_equal(self, expect_set, actual_iterable):
+        actual_set = set([str(s)[:10] for s in actual_iterable])
+        print(actual_set)
+        self.assertEqual(actual_set, expect_set)
+
+    def test_sma(self):
+        expect_golden_crosses = set([
+            "2018-07-11", 
+            "2017-03-23",
+            "2016-09-08",
+            "2014-07-07",
+            "2014-02-20",
+            "2013-04-15"
+        ])
+        expect_death_crosses = set([
+            "2018-06-11", 
+            "2016-11-01",
+            "2015-10-15",
+            "2014-04-24",
+            "2013-11-26",
+            "2012-12-03"
+        ])
+        stock = Stock("VRTX", self.data_source)
+        daily_series = stock.daily_series("2011-08-01", "2019-05-01")
+        golden_crosses = SMA(daily_series).golden_cross()
+        death_crosses = SMA(daily_series).death_cross()
+        self.assert_set_values_equal(expect_golden_crosses, golden_crosses)
+        self.assert_set_values_equal(expect_death_crosses, death_crosses)
+
+    def test_ema(self):
+        expect_golden_crosses = set([
+            "2017-03-28", 
+            "2015-11-18",
+            "2014-06-26",
+            "2013-12-27",
+            "2013-03-18",
+            "2012-03-13",
+            "2011-09-08",
+            "2011-08-24",
+        ])
+        expect_death_crosses = set([
+            "2016-01-12", 
+            "2015-10-12",
+            "2014-04-17",
+            "2013-12-06",
+            "2012-11-20",
+            "2011-10-10",
+            "2011-08-29",
+        ])
+        stock = Stock("VRTX", self.data_source)
+        daily_series = stock.daily_series("2011-08-01", "2019-05-01")
+        golden_crosses = EMA(daily_series).golden_cross()
+        death_crosses = EMA(daily_series).death_cross()
+        self.assert_set_values_equal(expect_golden_crosses, golden_crosses)
+        self.assert_set_values_equal(expect_death_crosses, death_crosses)
+
+    def test_pass(self):
+        pass
--- virgo_stock/indicators.py
+++ virgo_stock/indicators.py
@@ -1,21 +1,23 @@
 class MovingAverage:
-    def __init__(self, time_series, method_name):
+    def __init__(self, data_frame):
         """
         TimeSeries stores data in reverse order.
         """
-        self.ts = time_series
-        self.method = getattr(self.ts, method_name)
+        self.df = data_frame
+
+    def calculate(self, n, series_type='close', name=None):
+        raise NotImplementedError()
 
     def series_cross(self, n, k):
         """Finds the timestamps where n-day moving average breaking above k-day moving average.
         """
-        series_n = self.method(n).iloc[:,0]
-        series_k = self.method(k).iloc[:,0]
+        series_n = self.calculate(n)
+        series_k = self.calculate(k)
         crosses = []
-        for i in range(0, len(self.ts) - 1):
+        for i in range(0, len(self.df) - 1):
             if series_n[i + 1] < series_k[i + 1] and series_n[i] > series_k[i]:
                 crosses.append(i)
-        return self.ts.df['timestamp'][crosses]
+        return self.df.index[crosses]
 
     def golden_cross(self, short_term=50, long_term=200):
         """Finds the timestamps where short-term moving average breaking above long-term moving average.
@@ -31,11 +33,26 @@ class MovingAverage:
 
 class SMA(MovingAverage):
     """Simple Moving Average"""
-    def __init__(self, time_series):
-        super().__init__(time_series, 'sma')
+
+    def calculate(self, n, series_type='close', name=None):
+        if name is None:
+            name = 'SMA_%s' % n
+        series = self.df[series_type][::-1].rolling(n).mean()[::-1]
+        self.df[name] = series
+        return self.df.loc[:, name]
 
 
 class EMA(MovingAverage):
-    """Exponential Moving Average"""
-    def __init__(self, time_series):
-        super().__init__(time_series, 'ema')
+    """Exponential Moving Average
+
+    See Also:
+        http://pandas.pydata.org/pandas-docs/stable/computation.html#exponentially-weighted-windows
+    
+    """
+    
+    def calculate(self, n, series_type='close', name=None):
+        if name is None:
+            name = 'EMA_%s' % n
+        series = self.df[series_type][::-1].ewm(span=n).mean()[::-1]
+        self.df[name] = series
+        return self.df.loc[:, name]
