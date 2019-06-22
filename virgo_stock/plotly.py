c79e127c35553bed58b5417c5370f1776ebbc703
Qiu <qiuosier@gmail.com>
2019-06-21 23:03:30 -0400

    plotly: add module.

---
+++ virgo_stock/plotly.py
@@ -0,0 +1,69 @@
+from Aries.visual.plotly import PlotlyFigure
+
+class Candlestick(PlotlyFigure):
+    INCREASING_COLOR = '#4CAF50'
+    DECREASING_COLOR = '#E53935'
+
+    def __init__(self, data_frame, title):
+        self.df = data_frame
+        layout = dict(
+            xaxis=dict(
+                rangeslider=dict(
+                    visible=False
+                ),
+                title="",
+            ),
+            yaxis=dict(
+                title="",
+                domain=[0.25, 1.0],
+            ),
+            yaxis2=dict(
+                domain=[0.0, 0.2],
+            ),
+            margin=dict(
+                t=30,
+                b=30,
+                r=30,
+                l=30,
+            )
+        )
+        super().__init__(plotly_layout=layout)
+        self.candle_stick(
+            data_frame,
+            increasing=dict(
+                line=dict(
+                    color=self.INCREASING_COLOR
+                )
+            ),
+            decreasing=dict(
+                line=dict(
+                    color=self.DECREASING_COLOR
+                )
+            )
+        )
+        self.bar(
+            data_frame.index, 
+            data_frame.volume, 
+            "Volume", 
+            yaxis='y2',
+            marker=dict(
+                color=self.volume_colors()
+            )
+        )
+
+    def volume_colors(self):
+        colors = []
+        size = len(self.df.close)
+        for i in range(size):
+            if i < size - 1:
+                if self.df.close[i] >= self.df.close[i + 1]:
+                    colors.append(self.INCREASING_COLOR)
+                else:
+                    colors.append(self.DECREASING_COLOR)
+            else:
+                if self.df.close[i] >= self.df.open[i]:
+                    colors.append(self.INCREASING_COLOR)
+                else:
+                    colors.append(self.DECREASING_COLOR)
+        return colors
+    
