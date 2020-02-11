7eff31fea1286db3613f96980925eac77f433ca6
Qiu <qiuosier@gmail.com>
2020-02-10 19:39:05 -0500

    Update test_stock.py

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -7,10 +7,13 @@ import pandas as pd
 import os
 import sys
 from tests.base import TestWithAlphaVantage
+from ..virgo_stock import sp500, plotly
 
 
 class TestDataSource(TestWithAlphaVantage):
-    def assert_data_frame(self, actual_df, expect_df):
+    def assert_data_frame_index(self, actual_df, expect_df):
+        """Asserts if two pandas dataframes have the same index.
+        """
         # Check if the data frames have the same number of rows
         self.assertEqual(len(actual_df), len(expect_df), "Data frames have different number of rows.")
         # Check index
@@ -31,7 +34,7 @@ class TestDataSource(TestWithAlphaVantage):
         expect_df = pd.read_excel(daily_data_file, index_col=0)
         stock = self.get_stock("AAPL")
         actual_df = stock.daily_series("2015-01-01", "2017-01-01")
-        self.assert_data_frame(actual_df, expect_df)
+        self.assert_data_frame_index(actual_df, expect_df)
 
     def test_weekly_series(self):
         """Tests getting weekly series data.
@@ -40,7 +43,7 @@ class TestDataSource(TestWithAlphaVantage):
         expect_df = pd.read_excel(data_file, index_col=0)
         stock = self.get_stock("AAPL")
         actual_df = stock.weekly_series("2015-01-01", "2017-01-01")
-        self.assert_data_frame(actual_df, expect_df)
+        self.assert_data_frame_index(actual_df, expect_df)
 
     def test_monthly_series(self):
         """Tests getting monthly series data.
@@ -49,4 +52,15 @@ class TestDataSource(TestWithAlphaVantage):
         expect_df = pd.read_excel(data_file, index_col=0)
         stock = self.get_stock("AAPL")
         actual_df = stock.monthly_series("2015-01-01", "2017-01-01")
-        self.assert_data_frame(actual_df, expect_df)
+        self.assert_data_frame_index(actual_df, expect_df)
+
+    def test_download_sp500(self):
+        symbol_list = sp500.download_symbols()
+        self.assertGreaterEqual(len(symbol_list), 500)
+
+    def test_plot_candlestick(self):
+        """Tests if the candlestick chart can be plotted.
+        """
+        stock = self.get_stock("AAPL")
+        daily_data = stock.daily_series("2015-01-01", "2017-01-01")
+        html = plotly.Candlestick(daily_data).to_html()
